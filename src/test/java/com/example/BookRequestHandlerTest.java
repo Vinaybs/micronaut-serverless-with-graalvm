808b9f2c8f01f043c86eeb64fe4b1b102c133c20
Bathalu Shivakumar <Vinay.Shivakumar@team.telstra.com>
2020-09-03 15:45:00 +0530

    Changed micronaut.processing.module to micronaut-serverless-with-graalvm

--- src/test/java/com/example/BookRequestHandlerTest.java
+++ src/test/java/com/example/BookRequestHandlerTest.java
@@ -1,4 +1,5 @@
 package com.example;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterAll;
@@ -7,26 +8,26 @@ import org.junit.jupiter.api.Test;
 
 public class BookRequestHandlerTest {
 
-    private static BookRequestHandler bookRequestHandler;
+	private static BookRequestHandler bookRequestHandler;
 
-    @BeforeAll
-    public static void setupServer() {
-        bookRequestHandler = new BookRequestHandler(); 
-    }
+	@BeforeAll
+	public static void setupServer() {
+		bookRequestHandler = new BookRequestHandler();
+	}
 
-    @AfterAll
-    public static void stopServer() {
-        if (bookRequestHandler != null) {
-            bookRequestHandler.getApplicationContext().close(); 
-        }
-    }
+	@AfterAll
+	public static void stopServer() {
+		if (bookRequestHandler != null) {
+			bookRequestHandler.getApplicationContext().close();
+		}
+	}
 
-    @Test
-    public void testHandler() {
-        Book book = new Book();
-        book.setName("Building Microservices");
-        BookSaved bookSaved = bookRequestHandler.execute(book);  
-        assertEquals(bookSaved.getName(),book.getName());
-        assertNotNull(bookSaved.getIsbn());
-    }
+	@Test
+	public void testHandler() {
+		Book book = new Book();
+		book.setName("Building Microservices");
+		BookSaved bookSaved = bookRequestHandler.execute(book);
+		assertEquals(bookSaved.getName(), book.getName());
+		assertNotNull(bookSaved.getIsbn());
+	}
 }
